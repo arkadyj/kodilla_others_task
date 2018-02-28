7208a4ed540f6e46806b41f21a8022e605f67f77
arkadyj <szelaga@gmail.com>
2018-02-28 10:02:41 +0100

    zad_22_2

---
+++ zad_22_2.sql
@@ -0,0 +1,38 @@
+alter table books add column BESTSELLER boolean;
+
+drop procedure if exists UpdateBestsellers;
+
+delimiter $$
+
+create procedure UpdateBestsellers()
+begin
+	declare booksrent, days, bk_id int;
+	declare bookspermonth decimal(5,2);
+    declare finished int default 0;
+    declare all_books cursor for select book_id from books;
+    declare continue handler for not found set finished = 1;
+    open all_books;
+    while (finished = 0) do
+		fetch all_books into bk_id;
+        if (finished = 0) then
+			select count(*) from rents where book_id = bk_id
+            into booksrent;
+            select datediff(current_date(), min(rent_date)) from rents
+            where book_id = bk_id
+            into days;
+            set bookspermonth = booksrent / (days/30);
+            update books set test = bookspermonth where BOOK_ID = bk_id;            
+            if (bookspermonth >2) then            
+				update books set bestseller = true
+				where book_id = bk_id;
+            else
+				update books set bestseller = false
+				where book_id = bk_id;
+            end if;    
+            commit;
+         end if;
+	end while;
+    close all_books;
+end $$
+
+delimiter ;
